26775a6104b87b2b95884cfd6d4bc26b7ccc3a6e
bajajku <bajajku@sheridancollege.ca>
2023-09-14 17:17:45 -0400

    Initial commit

---
+++ app/src/main/java/sheridan/bajajku/paintapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package sheridan.bajajku.paintapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sheridan.bajajku.paintapp.ui.theme.PaintAPPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContentView(R.layout.activity_main)
+                }
+            }
+        }
+
+
